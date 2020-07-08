60e0f1a5666f48f6470e89aa1899661f815e984b
Danium <Kividanya964@gmail.com>
2020-07-08 21:19:58 +0300

    peopleReducer is added

---
+++ src/redux/reducers/peopleReducer.ts
@@ -0,0 +1,28 @@
+import { PeopleStore } from '../../types/store/store';
+import { ActionType } from '../../types/actions';
+import { SET_PEOPLE, SET_PERSON } from '../actions/actions';
+
+const initialState: PeopleStore = {
+  people: null,
+  person: null
+};
+
+const peopleReducer = (state = initialState, action: ActionType): PeopleStore => {
+  switch (action.type) {
+    case SET_PEOPLE: {
+      return {
+        ...state,
+        people: [...action.people]
+      };
+    }
+    case SET_PERSON: {
+      return {
+        ...state,
+        person: { ...action.person }
+      };
+    }
+    default: return state;
+  }
+};
+
+export default peopleReducer;
