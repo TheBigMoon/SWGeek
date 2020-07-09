b6b5181d1f00c2d26af5b3d01c2c6c6a8a02637f
Danium <Kividanya964@gmail.com>
2020-07-09 11:15:15 +0300

    personId is added to GetPerson action type

--- src/types/actions.ts
+++ src/types/actions.ts
@@ -9,7 +9,7 @@ export type PeopleActionType = SetPeople | SetPerson
 
 export interface GetPeoples {type: typeof GET_PEOPLE}
 export interface SetPeople {type: typeof SET_PEOPLE, people: Array<Person>}
-export interface GetPerson {type: typeof GET_PERSON}
+export interface GetPerson {type: typeof GET_PERSON, personId: number}
 export interface SetPerson {type: typeof SET_PERSON, person: Person}
 
 export type FilmsActionType = SetFilms | SetFilm
