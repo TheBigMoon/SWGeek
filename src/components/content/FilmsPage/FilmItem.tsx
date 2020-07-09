8d540f5edf58a5922d3a9594c09b72422a1f4df8
Danium <Kividanya964@gmail.com>
2020-07-09 21:10:07 +0300

    InfoBlock is added to all Items

--- src/components/content/FilmsPage/FilmItem.tsx
+++ src/components/content/FilmsPage/FilmItem.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import { Film } from '../../../types/entities/entities';
+import InfoBlock from '../infoBlock/InfoBlock';
 
 interface FilmProps {
   film: Film | null
 }
 
 const FilmItem: React.FC<FilmProps> = ({ film }) => {
-  const characters = film?.characters.map((character) => <div>{character}</div>);
-  const planets = film?.planets.map((planet) => <div>{planet}</div>);
-  const starships = film?.starships.map((starship) => <div>{starship}</div>);
-  const vehicles = film?.vehicles.map((vehicle) => <div>{vehicle}</div>);
-  const species = film?.species.map((race) => <div>{race}</div>);
-
   return (
     <div>
       <div>{film?.title}</div>
@@ -34,26 +29,11 @@ const FilmItem: React.FC<FilmProps> = ({ film }) => {
       <div>
         {`Release date: ${film?.release_date}`}
       </div>
-      <div>
-        <p>Characters:</p>
-        {characters}
-      </div>
-      <div>
-        <p>Planets:</p>
-        {planets}
-      </div>
-      <div>
-        <p>Starships:</p>
-        {starships}
-      </div>
-      <div>
-        <p>Vehicles:</p>
-        {vehicles}
-      </div>
-      <div>
-        <p>Species:</p>
-        {species}
-      </div>
+      <InfoBlock links={film?.characters} linksName="People" name="Characters" />
+      <InfoBlock links={film?.planets} linksName="Planet" name="Planets" />
+      <InfoBlock links={film?.starships} linksName="Starship" name="Starships" />
+      <InfoBlock links={film?.vehicles} linksName="Vehicle" name="Vehicles" />
+      <InfoBlock links={film?.planets} linksName="Race" name="Species" />
     </div>
   );
 };
--- src/components/content/Species/RaceItem.tsx
+++ src/components/content/Species/RaceItem.tsx
@@ -1,39 +1,26 @@
 import React from 'react';
 import { Race } from '../../../types/entities/entities';
+import InfoBlock from '../infoBlock/InfoBlock';
 
 interface RaceProps {
   race: Race | null
 }
 
-const RaceItem: React.FC<RaceProps> = ({ race }) => {
-  const people = race?.people.map((person) => <div>{person}</div>);
-  const films = race?.films.map((film) => <div>{film}</div>);
-  return (
-    <div>
-      <div>{`Name: ${race?.name}`}</div>
-      <div>{`Classification: ${race?.classification}`}</div>
-      <div>{`Designation: ${race?.designation}`}</div>
-      <div>{`Average height: ${race?.average_height}`}</div>
-      <div>{`Skin colors: ${race?.skin_colors}`}</div>
-      <div>{`Hair_colors: ${race?.hair_colors}`}</div>
-      <div>{`Eye_colors: ${race?.eye_colors}`}</div>
-      <div>{`Average_lifespan: ${race?.average_lifespan}`}</div>
-      <div>{`Homeworld: ${race?.homeworld}`}</div>
-      <div>{`Language: ${race?.language}`}</div>
-      <div>
-        <p>People:</p>
-        <div>
-          {people}
-        </div>
-      </div>
-      <div>
-        <p>Films:</p>
-        <div>
-          {films}
-        </div>
-      </div>
-    </div>
-  );
-};
+const RaceItem: React.FC<RaceProps> = ({ race }) => (
+  <div>
+    <div>{`Name: ${race?.name}`}</div>
+    <div>{`Classification: ${race?.classification}`}</div>
+    <div>{`Designation: ${race?.designation}`}</div>
+    <div>{`Average height: ${race?.average_height}`}</div>
+    <div>{`Skin colors: ${race?.skin_colors}`}</div>
+    <div>{`Hair_colors: ${race?.hair_colors}`}</div>
+    <div>{`Eye_colors: ${race?.eye_colors}`}</div>
+    <div>{`Average_lifespan: ${race?.average_lifespan}`}</div>
+    <div>{`Homeworld: ${race?.homeworld}`}</div>
+    <div>{`Language: ${race?.language}`}</div>
+    <InfoBlock links={race?.people} linksName="Race" name="People" />
+    <InfoBlock links={race?.films} linksName="Film" name="Films" />
+  </div>
+);
 
 export default RaceItem;
