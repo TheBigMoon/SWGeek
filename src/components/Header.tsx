3c6b372c8aa28af186204edca8d81509989f8340
Danium <Kividanya964@gmail.com>
2020-07-09 15:04:48 +0300

    simple Header is added

---
+++ src/components/Header.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => (
+  <div>
+    <NavLink exact to="/">
+      Films
+    </NavLink>
+    <NavLink exact to="/people">
+      People
+    </NavLink>
+    <NavLink exact to="/starships">
+      Starships
+    </NavLink>
+    <NavLink exact to="/vehicles">
+      Vehicles
+    </NavLink>
+    <NavLink exact to="/species">
+      Species
+    </NavLink>
+    <NavLink exact to="/planets">
+      Planets
+    </NavLink>
+  </div>
+);
+
+export default Header;
