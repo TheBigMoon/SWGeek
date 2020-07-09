4bfa0b39112e83b10cffdc419b46ec4d50e3f19b
Danium <Kividanya964@gmail.com>
2020-07-09 16:29:17 +0300

    App component is changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
+import Header from './components/Header';
+import FilmsContainer from './components/content/FilmsPage/FilmsContainer';
+import FilmContainer from "./components/content/FilmsPage/FilmContainer";
 
 function App() {
   return (
-    <div>
-      SWGeek
-    </div>
+    <>
+      <Header />
+      <Route exact path="/films" render={() => <FilmsContainer />} />
+      <Route exact path="/films/:filmId" render={() => <FilmContainer />} />
+      <Route exact path="/people">
+        People
+      </Route>
+      <Route exact path="/starships">
+        Starships
+      </Route>
+      <Route exact path="/vehicles">
+        Vehicles
+      </Route>
+      <Route exact path="/species">
+        Species
+      </Route>
+      <Route exact path="/planets">
+        Planets
+      </Route>
+    </>
   );
 }
 
