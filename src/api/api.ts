8865300f6a4f71449eaf12d23600756355554ac6
Danium <Kividanya964@gmail.com>
2020-07-09 23:00:30 +0300

    'page' prop is added to API methods

--- src/api/api.ts
+++ src/api/api.ts
@@ -13,41 +13,41 @@ const API = {
     return await instance.get(`people/${personId}/`)
       .then((response) => response.data);
   },
-  async getFilms() {
-    return await instance.get('films/')
-      .then((response) => response.data.results);
+  async getFilms(page: string) {
+    return await instance.get(`films/${page}`)
+      .then((response) => response.data);
   },
   async getFilm(filmId: number) {
     return await instance.get(`films/${filmId}/`)
       .then((response) => response.data);
   },
-  async getStarships() {
-    return await instance.get('starships/')
-      .then((response) => response.data.results);
+  async getStarships(page: string) {
+    return await instance.get(`starships/${page}`)
+      .then((response) => response.data);
   },
   async getStarship(starshipId: number) {
     return await instance.get(`starships/${starshipId}/`)
       .then((response) => response.data);
   },
-  async getVehicles() {
-    return await instance.get('vehicles/')
-      .then((response) => response.data.results);
+  async getVehicles(page: string) {
+    return await instance.get(`vehicles/${page}`)
+      .then((response) => response.data);
   },
   async getVehicle(vehicleId: number) {
     return await instance.get(`vehicles/${vehicleId}/`)
       .then((response) => response.data);
   },
-  async getSpecies() {
-    return await instance.get('species/')
-      .then((response) => response.data.results);
+  async getSpecies(page: string) {
+    return await instance.get(`species/${page}`)
+      .then((response) => response.data);
   },
   async getRace(raceId: number) {
     return await instance.get(`species/${raceId}/`)
       .then((response) => response.data);
   },
-  async getPlanets() {
-    return await instance.get('planets/')
-      .then((response) => response.data.results);
+  async getPlanets(page: string) {
+    return await instance.get(`planets/${page}`)
+      .then((response) => response.data);
   },
   async getPlanet(planetId: number) {
     return await instance.get(`planets/${planetId}/`)
