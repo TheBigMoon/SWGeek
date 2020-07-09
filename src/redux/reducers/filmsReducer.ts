c4a862f1121b8190517d93cbb15bbeab3c607062
Danium <Kividanya964@gmail.com>
2020-07-09 11:09:03 +0300

    filmsReducer is added

---
+++ src/redux/reducers/filmsReducer.ts
@@ -0,0 +1,28 @@
+import { FilmsStore } from '../../types/store/store';
+import { SET_FILM, SET_FILMS } from '../actions/actions';
+import { FilmsActionType } from '../../types/actions';
+
+const initialState: FilmsStore = {
+  films: null,
+  film: null
+};
+
+const filmsReducer = (state = initialState, action: FilmsActionType): FilmsStore => {
+  switch (action.type) {
+    case SET_FILMS: {
+      return {
+        ...state,
+        films: [...action.films]
+      };
+    }
+    case SET_FILM: {
+      return {
+        ...state,
+        film: action.film
+      };
+    }
+    default: return state;
+  }
+};
+
+export default filmsReducer;
