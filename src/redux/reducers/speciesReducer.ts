39727e5961c3605207f005f0ebe8340345eb6ccf
Danium <Kividanya964@gmail.com>
2020-07-09 11:54:39 +0300

    speciesReducer is added

---
+++ src/redux/reducers/speciesReducer.ts
@@ -0,0 +1,28 @@
+import { SpeciesStore } from '../../types/store/store';
+import { SET_RACE, SET_SPECIES } from '../actions/actions';
+import { SpeciesActionType } from '../../types/actions';
+
+const initialState: SpeciesStore = {
+  species: null,
+  race: null
+};
+
+const speciesReducer = (state = initialState, action: SpeciesActionType): SpeciesStore => {
+  switch (action.type) {
+    case SET_SPECIES: {
+      return {
+        ...state,
+        species: [...action.species]
+      };
+    }
+    case SET_RACE: {
+      return {
+        ...state,
+        race: action.race
+      };
+    }
+    default: return state;
+  }
+};
+
+export default speciesReducer;
