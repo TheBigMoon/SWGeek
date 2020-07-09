00f2c9036a73abc38c737126f8ae65e4f5c7f21a
Danium <Kividanya964@gmail.com>
2020-07-09 23:05:49 +0300

    Paginator is added to page

--- src/components/content/StarshipsPage/StarshipsContainer.tsx
+++ src/components/content/StarshipsPage/StarshipsContainer.tsx
@@ -4,21 +4,26 @@ import { App } from '../../../redux/store';
 import { Starship } from '../../../types/entities/entities';
 import { getStarships } from '../../../redux/actions/actions';
 import StarshipItem from './StarshipItem';
+import Paginator from '../common/Paginator';
 
 interface StateToProps {
-  starships: Array<Starship> | null
+  starships: Array<Starship> | null,
+  prevPage: string | null,
+  nextPage: string | null
 }
 
 interface DispatchToProps {
-  getStarships: () => void
+  getStarships: (page: string) => void
 }
 
 const StarshipsContainer: React.FC<StateToProps & DispatchToProps> = (
-  { starships, getStarships }
+  {
+    starships, prevPage, nextPage, getStarships
+  }
 ) => {
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getStarships());
+    dispatch(getStarships(''));
   }, [dispatch, getStarships]);
   const allStarships = starships?.map((starship) => <StarshipItem starship={starship} />);
   return (
@@ -27,12 +32,15 @@ const StarshipsContainer: React.FC<StateToProps & DispatchToProps> = (
       <div>
         {allStarships}
       </div>
+      <Paginator prevPage={prevPage} nextPage={nextPage} getContent={getStarships} />
     </div>
   );
 };
 
 const MapStateToProps = (state: App): StateToProps => ({
-  starships: state.starshipsPage.starships
+  starships: state.starshipsPage.starships,
+  prevPage: state.starshipsPage.prevPage,
+  nextPage: state.starshipsPage.nextPage
 });
 
 export default connect<StateToProps, DispatchToProps, {}, App>(
