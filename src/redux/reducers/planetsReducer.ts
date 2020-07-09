ea7fa853e149681fced6f13ea2d74a687f9e3083
Danium <Kividanya964@gmail.com>
2020-07-09 12:03:24 +0300

    planetsReducer is added

---
+++ src/redux/reducers/planetsReducer.ts
@@ -0,0 +1,28 @@
+import { PlanetsStore } from '../../types/store/store';
+import { SET_PLANET, SET_PLANETS } from '../actions/actions';
+import { PlanetsActionType } from '../../types/actions';
+
+const initialState: PlanetsStore = {
+  planets: null,
+  planet: null
+};
+
+const planetsReducer = (state = initialState, action: PlanetsActionType): PlanetsStore => {
+  switch (action.type) {
+    case SET_PLANETS: {
+      return {
+        ...state,
+        planets: [...action.planets]
+      };
+    }
+    case SET_PLANET: {
+      return {
+        ...state,
+        planet: action.planet
+      };
+    }
+    default: return state;
+  }
+};
+
+export default planetsReducer;
