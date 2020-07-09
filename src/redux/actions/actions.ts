5ef0b417274554c30254a3c7fc51e4b5d502e89f
Danium <Kividanya964@gmail.com>
2020-07-09 11:08:12 +0300

    film actions and action types is added

--- src/redux/actions/actions.ts
+++ src/redux/actions/actions.ts
@@ -1,14 +1,32 @@
 import {
-  GetPeoples, GetPerson, SetPeople, SetPerson
+  GetFilm,
+  GetFilms,
+  GetPeoples,
+  GetPerson,
+  SetFilm,
+  SetFilms,
+  SetPeople,
+  SetPerson
 } from '../../types/actions';
-import { Person } from '../../types/entities/entities';
+import { Film, Person } from '../../types/entities/entities';
 
 export const GET_PEOPLE = 'GET_PEOPLE';
 export const SET_PEOPLE = 'SET_PEOPLE';
 export const GET_PERSON = 'GET_PERSON';
 export const SET_PERSON = 'SET_PERSON';
+export const GET_FILMS = 'GET_FILMS';
+export const SET_FILMS = 'SET_FILMS';
+export const GET_FILM = 'GET_FILM';
+export const SET_FILM = 'SET_FILM';
 
+// PEOPLE ACTIONS
 export const getPeoples = (): GetPeoples => ({ type: GET_PEOPLE });
 export const setPeoples = (people: Array<Person>): SetPeople => ({ type: SET_PEOPLE, people });
 export const getPerson = (): GetPerson => ({ type: GET_PERSON });
-export const setPerson = (person: Person): SetPerson => ({ type: SET_PERSON, person});
+export const setPerson = (person: Person): SetPerson => ({ type: SET_PERSON, person });
+
+// FILMS ACTIONS
+export const getFilms = (): GetFilms => ({ type: GET_FILMS });
+export const setFilms = (films: Array<Film>): SetFilms => ({ type: SET_FILMS, films });
+export const getFilm = (filmId: number): GetFilm => ({ type: GET_FILM, filmId });
+export const setFilm = (film: Film): SetFilm => ({ type: SET_FILM, film });
--- src/types/actions.ts
+++ src/types/actions.ts
@@ -1,11 +1,20 @@
 import {
-  GET_PEOPLE, GET_PERSON, SET_PEOPLE, SET_PERSON
+  GET_FILM,
+  GET_FILMS,
+  GET_PEOPLE, GET_PERSON, SET_FILM, SET_FILMS, SET_PEOPLE, SET_PERSON
 } from '../redux/actions/actions';
-import { Person } from './entities/entities';
+import { Film, Person } from './entities/entities';
 
-export type ActionType = SetPeople | SetPerson
+export type PeopleActionType = SetPeople | SetPerson
 
 export interface GetPeoples {type: typeof GET_PEOPLE}
 export interface SetPeople {type: typeof SET_PEOPLE, people: Array<Person>}
 export interface GetPerson {type: typeof GET_PERSON}
 export interface SetPerson {type: typeof SET_PERSON, person: Person}
+
+export type FilmsActionType = SetFilms | SetFilm
+
+export interface GetFilms {type: typeof GET_FILMS}
+export interface SetFilms {type: typeof SET_FILMS, films: Array<Film>}
+export interface GetFilm {type: typeof GET_FILM, filmId: number}
+export interface SetFilm {type: typeof SET_FILM, film: Film}
