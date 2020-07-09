340593b1431f874f621b9a307c5db6b98436cb56
Danium <Kividanya964@gmail.com>
2020-07-09 11:27:39 +0300

    Starships action types is added

--- src/types/actions.ts
+++ src/types/actions.ts
@@ -1,9 +1,18 @@
 import {
   GET_FILM,
   GET_FILMS,
-  GET_PEOPLE, GET_PERSON, SET_FILM, SET_FILMS, SET_PEOPLE, SET_PERSON
+  GET_PEOPLE,
+  GET_PERSON,
+  GET_STARSHIP,
+  GET_STARSHIPS,
+  SET_FILM,
+  SET_FILMS,
+  SET_PEOPLE,
+  SET_PERSON,
+  SET_STARSHIP,
+  SET_STARSHIPS
 } from '../redux/actions/actions';
-import { Film, Person } from './entities/entities';
+import { Film, Person, Starship } from './entities/entities';
 
 export type PeopleActionType = SetPeople | SetPerson
 
@@ -18,3 +27,10 @@ export interface GetFilms {type: typeof GET_FILMS}
 export interface SetFilms {type: typeof SET_FILMS, films: Array<Film>}
 export interface GetFilm {type: typeof GET_FILM, filmId: number}
 export interface SetFilm {type: typeof SET_FILM, film: Film}
+
+export type StarshipsActionType = SetStarships | SetStarship
+
+export interface GetStarships {type: typeof GET_STARSHIPS}
+export interface SetStarships {type: typeof SET_STARSHIPS, starships: Array<Starship>}
+export interface GetStarship {type: typeof GET_STARSHIP, starshipId: number}
+export interface SetStarship {type: typeof SET_STARSHIP, starship: Starship}
