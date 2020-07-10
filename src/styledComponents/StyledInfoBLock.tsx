9a096b25857e8044ab7e43cac832a421dc0a89f8
Danium <Kividanya964@gmail.com>
2020-07-10 10:27:15 +0300

    InfoBlock component was styled

---
+++ src/styledComponents/StyledInfoBLock.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const StyledInfoBLock = styled.div`
+  display: inline-block;
+  width: auto;
+  
+  & div a {
+    text-decoration: none;
+    color: #66bb6a;
+    font-weight: 600;
+    margin-top: 5px;
+  }
+`;
+
+export const InfoName = styled.button`
+  border: none;
+  background-color: white;
+  outline: none;
+  color: #338a3e;
+  font-size: 16px;
+  font-weight: 600;
+  cursor: pointer;
+  padding-top: 5px;
+  text-align: justify;
+`;
