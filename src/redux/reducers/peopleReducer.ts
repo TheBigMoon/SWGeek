2a19fe56ffcf272ee971ed74ff8ad2762265f165
Danium <Kividanya964@gmail.com>
2020-07-11 15:33:49 +0300

    SORT_PEOPLE_BY_A_Z and SORT_PEOPLE_BY_Z_A cases is added

--- src/redux/reducers/peopleReducer.ts
+++ src/redux/reducers/peopleReducer.ts
@@ -1,6 +1,9 @@
 import { PeopleStore } from '../../types/store/store';
 import { PeopleActionType } from '../../types/actions/peopleActionsTypes';
-import { SET_PEOPLE, SET_PERSON } from '../../constants/actionTypeConstants';
+import {
+  SET_PEOPLE, SET_PERSON, SORT_PEOPLE_BY_A_Z, SORT_PEOPLE_BY_Z_A
+} from '../../constants/actionTypeConstants';
+import { sortHelper } from './reducerHelpers/sortHelper';
 
 const initialState: PeopleStore = {
   people: null,
@@ -14,9 +17,10 @@ const initialState: PeopleStore = {
 const peopleReducer = (state = initialState, action: PeopleActionType): PeopleStore => {
   switch (action.type) {
     case SET_PEOPLE: {
+      const people = sortHelper(state.sortByAZ, state.sortByZA, action.people);
       return {
         ...state,
-        people: [...action.people],
+        people: [...people],
         prevPage: action.prevPage,
         nextPage: action.nextPage
       };
@@ -25,6 +29,26 @@ const peopleReducer = (state = initialState, action: PeopleActionType): PeopleSt
       return {
         ...state,
         person: { ...action.person }
+      };
+    }
+    case SORT_PEOPLE_BY_A_Z: {
+      const people = state.people || [];
+      const sortedPeople = sortHelper(state.sortByAZ, state.sortByZA, people);
+      return {
+        ...state,
+        people: [...sortedPeople],
+        sortByAZ: action.sortByAZ,
+        sortByZA: false
+      };
+    }
+    case SORT_PEOPLE_BY_Z_A: {
+      const people = state.people || [];
+      const sortedPeople = sortHelper(state.sortByAZ, state.sortByZA, people);
+      return {
+        ...state,
+        people: [...sortedPeople],
+        sortByAZ: false,
+        sortByZA: action.sortByZA
       };
     }
     default: return state;
