2c2dfffce319bb88b0e30f69c5a53f3f274729b2
Danium <Kividanya964@gmail.com>
2020-07-11 15:32:28 +0300

    SortPeopleByAZ and SortPeopleByZA action types is added

--- src/types/actions/peopleActionsTypes.ts
+++ src/types/actions/peopleActionsTypes.ts
@@ -1,9 +1,14 @@
 import { Person } from '../entities/entities';
 import {
-  GET_PEOPLE, GET_PERSON, SET_PEOPLE, SET_PERSON
+  GET_PEOPLE,
+  GET_PERSON,
+  SET_PEOPLE,
+  SET_PERSON,
+  SORT_PEOPLE_BY_A_Z,
+  SORT_PEOPLE_BY_Z_A
 } from '../../constants/actionTypeConstants';
 
-export type PeopleActionType = SetPeople | SetPerson
+export type PeopleActionType = SetPeople | SetPerson | SortPeopleByAZ | SortPeopleByZA
 
 export interface GetPeople {type: typeof GET_PEOPLE, page: string}
 export interface SetPeople {
@@ -14,3 +19,5 @@ export interface SetPeople {
 }
 export interface GetPerson {type: typeof GET_PERSON, personId: number}
 export interface SetPerson {type: typeof SET_PERSON, person: Person}
+export interface SortPeopleByAZ {type: typeof SORT_PEOPLE_BY_A_Z, sortByAZ: boolean}
+export interface SortPeopleByZA {type: typeof SORT_PEOPLE_BY_Z_A, sortByZA: boolean}
