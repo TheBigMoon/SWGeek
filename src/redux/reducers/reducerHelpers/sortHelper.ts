6765f9399cdb2932f0926ff232cf3950ce753083
Danium <Kividanya964@gmail.com>
2020-07-11 15:21:54 +0300

    added generic type

--- src/redux/reducers/reducerHelpers/sortHelper.ts
+++ src/redux/reducers/reducerHelpers/sortHelper.ts
@@ -2,7 +2,7 @@ import {
   Film, Person, Planet, Race, Starship, Vehicle
 } from '../../../types/entities/entities';
 
-export function filmSorter(sortByAZ: boolean, sortByZA: boolean, items: Array<Film>) {
+export function filmSorter(sortByAZ: boolean, sortByZA: boolean, items: Array<Film>): Array<Film> {
   return sortByAZ
     ? [...items].sort((filmA, filmB) => (
       filmA.title.toLowerCase() < filmB.title.toLowerCase() ? -1 : 1))
@@ -13,7 +13,9 @@ export function filmSorter(sortByAZ: boolean, sortByZA: boolean, items: Array<Fi
 
 type ItemType = Person | Race | Planet | Starship | Vehicle
 
-export function sortHelpers(sortByAZ: boolean, sortByZA: boolean, items: Array<ItemType>) {
+export function sortHelper<T extends ItemType>(
+  sortByAZ: boolean, sortByZA: boolean, items: T[]
+): T[] {
   return sortByAZ
     ? [...items].sort((itemA, itemB) => (
       itemA.name.toLowerCase() < itemB.name.toLowerCase() ? -1 : 1))
