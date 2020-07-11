270321d3f09cd6791c26b4bc3c575d9b2daf6552
Danium <Kividanya964@gmail.com>
2020-07-11 10:38:56 +0300

    Paginator btn is reworked to NavLink

--- src/components/content/common/Paginator.tsx
+++ src/components/content/common/Paginator.tsx
@@ -1,32 +1,31 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 import { PageButton, StyledPaginator } from '../../../styledComponents/common/StyledPaginator';
 
 interface PaginatorProps {
   prevPage: string | null,
   nextPage: string | null,
-  getContent: (page: string) => void
 }
-const Paginator: React.FC<PaginatorProps> = ({ prevPage, nextPage, getContent }) => {
-  const dispatch = useDispatch();
-  const prev = prevPage || '';
-  const next = nextPage || '';
+const Paginator: React.FC<PaginatorProps> = ({ prevPage, nextPage }) => {
   return (
     <StyledPaginator>
-      <PageButton
-        disabled={!prev}
-        marginRight
-        onClick={() => dispatch(getContent(prev.replace(/http:\/\/swapi\.dev\/api\/.+\//g, '')))}
-      >
+      {
         prevPage
-      </PageButton>
-      <PageButton
-        disabled={!next}
-        marginRight={false}
-        onClick={() => dispatch(getContent(next.replace(/http:\/\/swapi\.dev\/api\/.+\//g, '')))}
-      >
+          ? (
+            <PageButton to={prevPage.replace(/http:\/\/swapi\.dev\/api\/.+\//g, '')}>
+              prevPage
+            </PageButton>
+          )
+          : null
+      }
+      {
         nextPage
-      </PageButton>
+          ? (
+            <PageButton to={nextPage.replace(/http:\/\/swapi\.dev\/api\/.+\//g, '')}>
+              nextPage
+            </PageButton>
+          )
+          : null
+      }
     </StyledPaginator>
   );
 };
--- src/styledComponents/common/StyledPaginator.ts
+++ src/styledComponents/common/StyledPaginator.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
 export const StyledPaginator = styled.div`
   display: flex;
@@ -8,20 +9,18 @@ export const StyledPaginator = styled.div`
   margin: 15px 0;
 `;
 
-interface PageButtonProps {
-  marginRight: boolean
-}
-
-export const PageButton = styled.button`
-  margin-right: ${(props: PageButtonProps) => (props.marginRight ? '25px' : '0')};
+export const PageButton = styled(NavLink)`
+  margin: 0 15px;
   background-color: #66bb6a;
   border: none;
-  padding: 10px 25px;
+  padding: 7px 25px;
   outline: none;
   border-radius: 5px;
   cursor: pointer;
+  color: #000000;
   font-size: 14px;
   font-weight: 600;
+  text-decoration: none;
   
   &:active {
     background-color: #aed581;
