2293416ffae8722273f9b311a337c84f162f4366
Danium <Kividanya964@gmail.com>
2020-07-11 14:53:48 +0300

    SORT_FILMS_BY_A_Z and SORT_FILMS_BY_Z_A cases is added

--- src/redux/reducers/filmsReducer.ts
+++ src/redux/reducers/filmsReducer.ts
@@ -1,24 +1,50 @@
 import { FilmsStore } from '../../types/store/store';
 import { FilmsActionType } from '../../types/actions/filmsActionsTypes';
-import { SET_FILM, SET_FILMS } from '../../constants/actionTypeConstants';
+import {
+  SET_FILM, SET_FILMS, SORT_FILMS_BY_A_Z, SORT_FILMS_BY_Z_A
+} from '../../constants/actionTypeConstants';
+import { filmSorter } from './reducerHelpers/sortHelpers';
 
 const initialState: FilmsStore = {
   films: null,
-  film: null
+  film: null,
+  sortByAZ: false,
+  sortByZA: false
 };
 
 const filmsReducer = (state = initialState, action: FilmsActionType): FilmsStore => {
   switch (action.type) {
     case SET_FILMS: {
+      const films = filmSorter(state.sortByAZ, state.sortByZA, action.films);
       return {
         ...state,
-        films: [...action.films]
+        films
       };
     }
     case SET_FILM: {
       return {
         ...state,
         film: action.film
+      };
+    }
+    case SORT_FILMS_BY_A_Z: {
+      const films = state.films || [];
+      const sortedFilms = filmSorter(state.sortByAZ, state.sortByZA, films);
+      return {
+        ...state,
+        films: [...sortedFilms],
+        sortByAZ: action.sortByAZ,
+        sortByZA: false
+      };
+    }
+    case SORT_FILMS_BY_Z_A: {
+      const films = state.films || [];
+      const sortedFilms = filmSorter(state.sortByAZ, state.sortByZA, films);
+      return {
+        ...state,
+        films: [...sortedFilms],
+        sortByZA: action.sortByZA,
+        sortByAZ: false
       };
     }
     default: return state;
