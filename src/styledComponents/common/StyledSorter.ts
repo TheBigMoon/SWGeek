38cbb2ccec6969bdd5d9afa2dc082e686b6db707
Danium <Kividanya964@gmail.com>
2020-07-11 17:09:46 +0300

    StyledSorter is added

---
+++ src/styledComponents/common/StyledSorter.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const StyledSorter = styled.div`
+  padding-left: 25px;
+`;
+
+export const SorterButton = styled.button`
+  padding: 3px 15px;
+  margin-left: 8px;
+  background-color: #66bb6a;
+  border: none;
+  outline: none;
+  border-radius: 5px;
+  cursor: pointer;
+  
+  &:active {
+    background-color: #aed581;
+  } 
+`;
