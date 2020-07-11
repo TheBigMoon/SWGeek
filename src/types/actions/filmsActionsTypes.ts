a54d43f589167c0428a03c6adde87e5be9e3b24c
Danium <Kividanya964@gmail.com>
2020-07-11 13:51:06 +0300

    types of SortByAZ and SortByZA action types is changed

--- src/types/actions/filmsActionsTypes.ts
+++ src/types/actions/filmsActionsTypes.ts
@@ -1,9 +1,9 @@
 import { Film } from '../entities/entities';
 import {
-  GET_FILM, GET_FILMS, SET_FILM, SET_FILMS, SORT_BY_A_Z, SORT_BY_Z_A
+  GET_FILM, GET_FILMS, SET_FILM, SET_FILMS, SORT_FILMS_BY_A_Z, SORT_FILMS_BY_Z_A
 } from '../../constants/actionTypeConstants';
 
-export type FilmsActionType = SetFilms | SetFilm
+export type FilmsActionType = SetFilms | SetFilm | SortByZA | SortByAZ
 
 export interface GetFilms {type: typeof GET_FILMS, page: string}
 export interface SetFilms {
@@ -14,5 +14,5 @@ export interface SetFilms {
 }
 export interface GetFilm {type: typeof GET_FILM, filmId: number}
 export interface SetFilm {type: typeof SET_FILM, film: Film}
-export interface SortByAZ {type: typeof SORT_BY_A_Z, sortByAZ: boolean}
-export interface SortByZA {type: typeof SORT_BY_Z_A, sortByZA: boolean}
+export interface SortByAZ {type: typeof SORT_FILMS_BY_A_Z, sortByAZ: boolean}
+export interface SortByZA {type: typeof SORT_FILMS_BY_Z_A, sortByZA: boolean}
